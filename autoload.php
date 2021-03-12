245546aa2f0cec2b05363e9696359c537ab552fa
inidwiii <dwigamers30@gmail.com>
2021-03-12 15:35:55 +0800

    Adding files autoloader
    Modifying autoload.php and config.json so that the files autoloader feature can work

--- autoload.php
+++ autoload.php
@@ -3,15 +3,25 @@
 $config = json_decode(file_get_contents(realpath(__DIR__ . '/config.json')));
 
 try {
+    foreach ($config->autoload->files as $file) {
+        $fileAbsPath = realpath(__DIR__ . DIRECTORY_SEPARATOR . $file);
+
+        if (!$fileAbsPath) {
+            throw new \InvalidArgumentException("Can't find file at '{$file}'.");
+        }
+
+        require $fileAbsPath;
+    }
+
     spl_autoload_register(function (string $className) use ($config) {
         $classBase = @explode('\\', $className)[0] . '\\';
         $classPath = $className;
 
-        if (!property_exists($config->autoload->class, $classBase)) {
-            throw new \InvalidArgumentException("Can't find class with name '{$className}'");
+        if (!property_exists($config->autoload->classes, $classBase)) {
+            throw new \InvalidArgumentException("Can't find class with name '{$className}'.");
         }
 
-        $classPath = str_replace($classBase, $config->autoload->class->{$classBase}, $classPath);
+        $classPath = str_replace($classBase, $config->autoload->classes->{$classBase}, $classPath);
         $classPath = strtr($classPath, '\\/', DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR);
         $classPath = realpath(__DIR__ . DIRECTORY_SEPARATOR . $classPath . '.php');
 
