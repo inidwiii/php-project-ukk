ee38711aeb4cd541c27e886f87abd77b35426104
inidwiii <dwigamers30@gmail.com>
2021-03-14 21:09:17 +0800

    Refactoring Route.php
    Adding comment and fix redundant logic

--- src/Core/Route.php
+++ src/Core/Route.php
@@ -8,17 +8,40 @@ class Route
 
     const METHOD_POST = 'post';
 
+    /**
+     * Hold the current registered route
+     * @var null|string $_current
+     */
     private $_current;
 
-
+    /**
+     * Hold the middlewares attached to the route
+     * @var array $_middlewareMap
+     */
     private $_middlewareMap = [];
 
+    /**
+     * Hold the alias name of the routes
+     * @var array $_namedMap
+     */
     private $_namedMap = [];
 
+    /**
+     * Hold the request object instance
+     * @var \Illuminate\Core\Request $_request
+     */
     private $_request;
 
+    /**
+     * Hold the response object instance
+     * @var \Illuminate\Core\Response $_response
+     */
     private $_response;
 
+    /**
+     * Hold all the routes registered
+     * @var array $_routeMap
+     */
     private $_routeMap = [];
 
     public function __construct(Request $request, Response $response)
@@ -27,6 +50,10 @@ class Route
         $this->_response = $response;
     }
 
+    /**
+     * Capture and registering route into routeMap
+     * @return mixed|void
+     */
     public function capture()
     {
         $this->_current = null;
@@ -34,43 +61,101 @@ class Route
         $requestMethod = $this->_request->method();
         $requestPath = $this->_request->path();
 
-        foreach ($this->_routeMap as $key => $route) {
+        foreach ($this->_routeMap as $routeId => $route) {
             if (preg_match($route['reg'], $requestPath, $routeParams) && $route['method'] === $requestMethod) {
-                $routeController = $route['callback']['controller'];
-                $routeMethod = $route['callback']['method'];
-                $routeMiddlewares = $this->_middlewareMap[$key];
-                $routeParams = array_slice($routeParams, 1);
+                extract($this->extractRouteData($routeId, $route, $routeParams));
 
-                var_dump($this->_middlewareMap);
-
-                if (class_exists($routeController) === false) {
-                    throw new \InvalidArgumentException("Can't find controller '{$routeController}'");
-                }
-
-                if (method_exists($routeController, $routeMethod) === false) {
-                    throw new \InvalidArgumentException("Can't find method '" . get_class($routeController) . "{$routeMethod}'");
-                }
+                $this->_response->header('X-App-Name', app(Config::class)->get('app.name'));
+                $this->_response->status(200);
 
                 return app()->dispatch($routeController, $routeMethod, ...$routeParams);
             }
         }
+
+        $this->_response->status(404);
+        die('404 - Not Found');
     }
 
+    /**
+     * Registering new route with method 'GET'
+     * @param string $url
+     * @param array $callback
+     * @return \Illuminate\Core\Route|self
+     */
     public function get($url, $callback)
     {
         return $this->registerRoute($url, $callback, self::METHOD_GET);
     }
 
+    /**
+     * Registering new route with method 'POST'
+     * @param string $url
+     * @param array $callback
+     * @return \Illuminate\Core\Route|self
+     */
+    public function post($url, $callback)
+    {
+        return $this->registerRoute($url, $callback, self::METHOD_POST);
+    }
+
+    /**
+     * Registering middlewares toward the current route
+     * @param string[] $middlewares
+     * @return \Illuminate\Core\Route|self
+     */
     public function middleware(...$middlewares)
     {
         return $this->registerRouteMiddleware($this->_current, $middlewares);
     }
 
+    /**
+     * Registering an alias name toward the current route
+     * @param string $name
+     * @return \Illuminate\Core\Route|self
+     */
     public function name($name)
     {
         return $this->registerRouteName($this->_current, $name);
     }
 
+    /**
+     * Collect and extracting required data from current matched route
+     * @param string $routeId
+     * @param array $routeData
+     * @param array $routeParams
+     * @return array
+     */
+    private function extractRouteData($routeId, $routeData, $routeParams)
+    {
+        return [
+            'routeController' => $routeData['callback']['controller'],
+            'routeMethod' => $routeData['callback']['method'],
+            'routeMiddleware' => $this->_middlewareMap[$routeId],
+            'routeParams' => array_slice($routeParams, 1)
+        ];
+    }
+
+    /**
+     * Extracting or creating route regex from the given url
+     * @param string $url
+     * @return string
+     */
+    private function extractRouteRegex($url)
+    {
+        $reg = rtrim($url, '/');
+        $reg = preg_replace('/(\/\{\w+\?\})/i', '(?:/([^/]+?))?', $reg);
+        $reg = preg_replace('/(\{\w+\})/i', '(?:([^/]+?))', $reg);
+        $reg = '/^' . str_replace('/', '\\/', $reg) . '\\/?$/';
+
+        return $reg;
+    }
+
+    /**
+     * Registering new middleware
+     * @param string $url
+     * @param array $middlewares
+     * @return \Illuminate\Core\Route|self
+     */
     private function registerRouteMiddleware($url, $middlewares)
     {
         if (is_null($url) || !$url) {
@@ -84,6 +169,12 @@ class Route
         return $this;
     }
 
+    /**
+     * Registering alias name
+     * @param string $url
+     * @param string $name
+     * @return \Illuminate\Core\Route|self
+     */
     private function registerRouteName($url, $name)
     {
         if (is_null($url) || !$url) {
@@ -94,17 +185,20 @@ class Route
         return $this;
     }
 
+    /**
+     * Registering nw route
+     * @param string $url
+     * @param string $callback
+     * @param string $method
+     * @return \Illuminate\Core\Route|self
+     */
     private function registerRoute($url, $callback, $method)
     {
-        $reg = rtrim($url, '/');
-        $reg = preg_replace('/(\/\{\w+\?\})/i', '(?:/([^/]+?))?', $reg);
-        $reg = preg_replace('/(\{\w+\})/i', '(?:([^/]+?))', $reg);
-        $reg = '/^' . str_replace('/', '\\/', $reg) . '\\/?$/i';
 
         $this->_current = $url;
         $this->_routeMap[$url] = [
             'url' => $this->_request->base($url),
-            'reg' => $reg,
+            'reg' => $this->extractRouteRegex($url),
             'callback' => [
                 'controller' => $callback[0],
                 'method' => $callback[1]
